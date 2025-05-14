d3bcfd2e1793579ebc32bc64b6cb939e9a5675a2
David Belais <david@belais.me>
2025-05-14 09:34:18 -0700

    *

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -7,10 +7,17 @@ PACKAGE_NAME_REGEX: str = r"^[a-z][._a-z0-9]*[a-z0-9]$"
 PACKAGE_NAME_PATTERN: Pattern = re.compile(PACKAGE_NAME_REGEX)
 PROJECT_NAME: str = "{{cookiecutter.project_name}}"
 PACKAGE_NAME: str = "{{cookiecutter.package}}"
+PACKAGE_DIRECTORY: str = "{{cookiecutter.package_directory}}"
 
 
 def main() -> None:
     message: str
+    if not PACKAGE_DIRECTORY.startswith("src/"):
+        message = (
+            f"{PACKAGE_DIRECTORY!r} is not a valid package directory. "
+            "Package directories must start with 'src/'."
+        )
+        raise ValueError(message)
     if not PROJECT_NAME_PATTERN.match(PROJECT_NAME):
         message = (
             f"{PROJECT_NAME!r} is not a valid project name. "
